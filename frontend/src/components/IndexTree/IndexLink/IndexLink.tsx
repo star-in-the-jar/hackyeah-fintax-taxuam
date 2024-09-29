6ec430848ac92286b6d600106f5e98a341127734
teawithsand <teawithsand teawithsand@gmail.com>
2024-09-29 02:15:45 +0000

    Added values preview on tree view

--- frontend/src/components/IndexTree/IndexLink/IndexLink.tsx
+++ frontend/src/components/IndexTree/IndexLink/IndexLink.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 export interface IndexLinkProps {
   title: string;
+  value?: string | null
   children?: React.ReactNode;
 }
 
@@ -12,11 +13,11 @@ const scrollToSection = (targetId: string) => {
   }
 };
 
-const IndexLink: React.FC<IndexLinkProps> = ({ title, children }) => {
+const IndexLink: React.FC<IndexLinkProps> = ({ title, children, value }) => {
   return (
     <div>
       <button onClick={() => scrollToSection(title)} className="block py-1 px-2 hover:bg-gray-100 rounded w-full">
-        <h3 className='text-left'>{title}</h3>
+        <h3 className='text-left flex justify-between'>{value ? <><span>{title}</span> <b>{value}</b></> : <span>{title}</span>}</h3>
       </button>
       {children}
     </div>
