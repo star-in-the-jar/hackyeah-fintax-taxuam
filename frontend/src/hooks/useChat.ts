e98fafdcdb2d927af53fba12f521f9c63fdc29de
teawithsand <teawithsand teawithsand@gmail.com>
2024-09-29 04:09:59 +0000

    un-mocked chat

--- frontend/src/hooks/useChat.ts
+++ frontend/src/hooks/useChat.ts
@@ -7,24 +7,15 @@ export const useChat = () => {
     cb: (text: string) => void
   ): Promise<Message> => {
     // PRZEMEK JEBNIJ TO POTEM
-    // const response = await fetch('http://localhost:3001/messages', {
+    // const response = await fetch('http://localhost:3001/messages',
 
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve({
-          role: "assistant",
-          content: "Return from server",
-        });
-      }, 200);
-    });
-
-    // if(!cb) throw new Error("NIE DZIALA")
-    // return fetchChatStream(
-    //   {
-    //     elements: messages,
-    //   },
-    //   cb
-    // );
+    if (!cb) throw new Error("NIE DZIALA");
+    return fetchChatStream(
+      {
+        elements: messages,
+      },
+      cb
+    );
   };
 
   return {
